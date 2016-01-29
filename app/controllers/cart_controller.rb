83906cf2674be5bd37c5c4f1611ca60cb0741740
TheCM <remigiusz.zamiara@gmail.com>
2016-01-29 13:45:23 +0100

    Implement cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,16 @@
 class CartController < ApplicationController
-  def :show
+  def show
+    @current_order = session[:order_id]
+    @order_items = OrderItem.all
+    @sum = 0
+  end
+
+  def payment
+  end
+
+  def thanks
+    @current_order = Order.new({:first_name => "newName", :last_name => "newLastName", :address => "address", :email => "mail@mail.com", :status => "not initiated"})
+    @current_order.save
+    session[:order_id] = @current_order.attributes['id']
   end
 end
